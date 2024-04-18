a44faf73da525c15d53c58c0d6bbefe6c48f826f
zymxer <ilyayan1756@gmail.com>
2024-04-18 23:18:04 +0200

    Танки на отдельных потоках (тупые)

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using Pathfinding;
 using Unity.VisualScripting;
+using UnityEngine.Serialization;
 
 public class Tank : MonoBehaviour
 {
-    //const during game
+    //CONST
+    [Header("Tank Parameters")]
     [SerializeField] private float speed;
     [SerializeField] private float rotationSpeed;
     [SerializeField] private float rotationThreshold;
@@ -15,15 +18,33 @@ public class Tank : MonoBehaviour
     [SerializeField] private float shotCooldown;
     [SerializeField] private float attackRange;
     
+    [Space] [Header("Navigation")] 
+    [SerializeField] private float nextPointDistance = 0.1f;
+
+    [FormerlySerializedAs("teamNumber")]
+    [Space] [Header("Team")] 
+    [SerializeField] private int _teamNumber;
+    [SerializeField] private int _tankNumber;
+    [SerializeField] private GameObject _flag;
+    
     private Timer _shotTimer;
     private Seeker _seeker;
-    [SerializeField] private float nextPointDistance = 0.1f;
     
-    //variable
+    
+    private Thread _thread;
+    private bool _threadStopped = false;
+    private AutoResetEvent _resetEvent;
+    private readonly object _lockObject = new object();
+    
+    //VARIABLES
     private Vector3 _position;
     private Quaternion _rotation;
     
+    private bool _pathFinished = false;
+    private bool _shoots = false;
+    
     private Path _path;
+    
     private Vector3 _target;
     private int _currentPoint;
     private Vector3 _direction;
@@ -32,106 +53,12 @@ public class Tank : MonoBehaviour
 
     private bool _canMove = true;
     private bool _triggered = false;
-
-
-    public Vector3 GetPosition()
-    {
-        return _position;
-    }
-
-    //synchronize
-    public void SetPosition(Vector2 value)
-    {
-        _position = value;
-    }
-
-    private void OnPathGenerationComplete(Path path)
-    {
-        if (!path.error)
-        {
-            _path = path;
-            _currentPoint = 0;
-        }
-    }
-
-    //Thread function
-    private void Run()
-    {
-        UpdatePath();
-        if (_path != null)
-        {
-            UpdateDirection();
-            UpdateRotation();
-            if (_canMove)
-            {
-                UpdatePosition();
-            }
-
-            if (_triggered)
-            {
-                _canMove = false;
-                Shoot();
-            }
-        }
-    }
-
-    private void UpdateDirection()
-    {
-        _direction = (_path.vectorPath[_currentPoint] - _position).normalized;
-        _angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
-    }
     
-    private void UpdatePath()
-    {
-        _distance = Vector2.Distance(_position, _path.vectorPath[_currentPoint]);
-        if (_distance < nextPointDistance)
-        {
-            _currentPoint++;
-        }
-        
-        if (ReachedEndCheck())
-        {
-            UpdateTarget(Field.Instance.GetRandomPoint());
-        }
-    }
+    //CHANGED IN MAIN THREAD
+    private float _deltaTime;
 
-    private void Shoot()
-    {
-        if (!_shotTimer.IsActive())
-        {
-            Instantiate(missile, _position, _rotation);
-            _shotTimer.Activate();   
-        }
-    }
-
-    private void UpdateRotation()
-    {
-        Quaternion targetRotation = Quaternion.Euler(0, 0, _angle - 90f);
-        _rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        if (Quaternion.Angle(_rotation, targetRotation) > rotationThreshold)
-        {
-            _canMove = false;
-        }
-        else
-        {
-            _canMove = true;
-        }
-    }
-
-    private void UpdatePosition()
-    {
-        _position += _direction * (speed * Time.deltaTime);
-    }
-
-    private void UpdateTarget(Vector3 value)
-    {
-        _target = value;
-        _seeker.StartPath(_position, _target, OnPathGenerationComplete);
-        _currentPoint = 0;
-    }
-
-
-    void Start()
+    
+    private void Start()
     {
         _position = transform.position;
         
@@ -144,30 +71,145 @@ public class Tank : MonoBehaviour
         _shotTimer = gameObject.AddComponent<Timer>();
         _shotTimer.SetTimer(shotCooldown);
 
-    }
+        _resetEvent = new AutoResetEvent(false);
+        _thread = new Thread(Run);
+        _thread.Start();
 
-    // Update is called once per frame
-    void Update()
+    }
+    
+    private void Update()
     {
+        _resetEvent.Set(); // allows thread function to continue executing
         if (_path == null)
             return;
         
-        Run();
+        _deltaTime = Time.deltaTime;
+        
+        if (_pathFinished)
+        {
+            UpdateTarget(Field.Instance.GetRandomPoint());
+            _pathFinished = false;
+        }
+
+        if (_shoots)
+        {
+            Shoot();
+        }
         
         transform.rotation = _rotation;
         transform.position = _position;
     }
-
-    private bool ReachedEndCheck()
+    
+    private void ThreadFunction()
     {
-        return _currentPoint >= _path.vectorPath.Count; 
+        _resetEvent.WaitOne();  // waits for signal from Update()
+        if (_pathFinished)
+            return;
+        UpdatePath();
+        if (_pathFinished)
+            return;
+        if (_path == null) 
+            return;
+        
+        UpdateDirection();
+        UpdateRotation();
+        if (_canMove)
+        {
+            UpdatePosition();
+        }
+
+        if (_triggered)
+        {
+            _canMove = false;
+            _shoots = true;
+        }
+    }
+    
+    private void OnPathGenerationComplete(Path path)
+    {
+        if (!path.error)
+        {
+            _path = path;
+            _currentPoint = 0;
+        }
+    }
+    
+    private void UpdateDirection()
+    {
+        _direction = (_path.vectorPath[_currentPoint] - _position).normalized;
+        _angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+    }
+    
+    private void UpdatePath()
+    {
+        if(_path == null)
+            return;
+        _distance = Vector2.Distance(_position, _path.vectorPath[_currentPoint]);
+        if (_distance < nextPointDistance)
+        {
+            _currentPoint++;
+        }
+
+        ReachedEndCheck();
+    }
+
+    private void Shoot()
+    {
+        if (!_shotTimer.IsActive())
+        {
+            Instantiate(missile, _position, _rotation);
+            _shotTimer.Activate();
+            _shoots = false;
+        }
+    }
+
+    private void UpdateRotation()
+    {
+        Quaternion targetRotation = Quaternion.Euler(0, 0, _angle - 90f);
+        _rotation = Quaternion.Lerp(_rotation, targetRotation, rotationSpeed * _deltaTime);
+        if (Quaternion.Angle(_rotation, targetRotation) > rotationThreshold)
+        {
+            _canMove = false;
+        }
+        else
+        {
+            _canMove = true;
+        }
+    }
+
+    private void UpdatePosition()
+    {
+        _position += _direction * (speed * _deltaTime);
+    }
+
+    private void UpdateTarget(Vector3 value)
+    {
+        _target = value;
+        _seeker.StartPath(_position, _target, OnPathGenerationComplete);
+        _currentPoint = 0;
+    }
+    
+    private void ReachedEndCheck()
+    {
+        if(_currentPoint >= _path.vectorPath.Count)
+        {
+            _pathFinished = true;
+        }
+    }
+    
+    private void Run()
+    {
+        while (!_threadStopped)  
+        {  
+            ThreadFunction();  
+        } 
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Tank"))
         {
-            UpdateTarget(other.transform.position);
+            //UpdateTarget(other.transform.position);
             _triggered = true;
         }
     }
@@ -179,5 +221,17 @@ public class Tank : MonoBehaviour
             _canMove = true;
             _triggered = false;
         }
+    }
+
+    private void OnDestroy()
+    {
+        _thread.Abort();
+        _threadStopped = true;
+    }
+
+    private void OnApplicationQuit()
+    {
+        _thread.Abort();
+        _threadStopped = true;
     }
 }
